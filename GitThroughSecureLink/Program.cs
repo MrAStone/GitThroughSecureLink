6a72baff749e17a49884c3c99cb25ad6b30c16fa
Al Stone <astone@heckgrammar.co.uk>
2024-03-12 13:26:14 +0000

    Add project files.

---
+++ GitThroughSecureLink/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitThroughSecureLink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Change the cryptographic network provider to Sercure Channel");
+        }
+    }
+}
